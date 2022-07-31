3e164991a0a55399d6d9acc567038afaa23ad2d8
Munikiran N <69495681+narmuni123@users.noreply.github.com>
2022-07-31 17:47:10 +0530

    certificate verify error resolved and getting data till home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'dart:io';
+
 import 'package:altor_assignment/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -12,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Altor Assignment',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:altor_assignment/model/about_model.dart';
+import 'package:altor_assignment/services/rest_api.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,10 +15,43 @@ class _HomeScreenState extends State<HomeScreen> {
 
   AboutModel? aboutModel;
 
+  final ScrollController _scrollController = ScrollController();
+
+  List itemList = [];
+
+  bool hasMore = true;
+
   bool isLoading = false;
+
+  RestApi restApi = RestApi();
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  Future<dynamic> getData() async{
+    if(isLoading) return ;
+    isLoading = true;
+
+    final response = await restApi.getData();
+
+    if(response.statusCode == 200){
+      final newItems = json.decode(response.body);
+      setState(() {
+        isLoading = false;
+        aboutModel = AboutModel.fromJson(newItems);
+        itemList = aboutModel!.rows;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SizedBox(),
+    );
   }
 }
